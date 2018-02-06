749db3a22ce4d138f6d50ad6d07d19abe96bab8c
Yanghwa <kellerj87@hotmail.com>
2018-02-06 13:58:17 -0500

    delete image

--- cloneinsta/images/views.py
+++ cloneinsta/images/views.py
@@ -167,6 +167,14 @@ class ModerateComments(APIView):
 
 class ImageDetail(APIView):
 
+    def find_own_image(self, image_id, user):
+        
+        try:
+            image = models.Image.objects.get(id=image_id, creator=user)
+            return image
+        except models.Image.DoesNotExist:
+            return None
+
     def get(self, request, image_id, format=None):
 
         user = request.user
@@ -184,9 +192,9 @@ class ImageDetail(APIView):
 
         user = request.user
 
-        try:
-            image = models.Image.objects.get(id=image_id, creator=user)
-        except models.Image.DoesNotExist:
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
 
         serializer = serializers.InputImageSerializer(image, data=request.data, partial=True)
@@ -199,3 +207,14 @@ class ImageDetail(APIView):
 
         else:
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, image_id, format=None):
+        
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        image.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
