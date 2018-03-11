a6044d96b6aa102f8ff51b36b305b3e45dfbac9b
Yanghwa <kellerj87@hotmail.com>
2018-03-10 20:04:17 -0500

    add dummy auth for apps

--- frontend/src/components/App/presenter.js
+++ frontend/src/components/App/presenter.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
-import styles from './styles.scss';
-import Footer from 'component/Footer';
+import './styles.scss';
+import Footer from 'components/Footer';
+import Auth from 'components/Auth';
 
 const App = props => [
   props.isLoggedIn ? <PrivateRoute key={2} /> : <PublicRoutes key={2} />,
   <Footer key={3} />
-]
+];
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired
+};
 
 const PrivateRoute = props => (
   <Switch>
     <Route exact path="/" render={() => "feed"} />
     <Route exact path="/explore" render={() => "explore"} />
   </Switch>
-)
+);
 
 const PublicRoutes = props => (
   <Switch>
-    <Route exact path="/" render={() => "login"} />
+    <Route exact path="/" component={Auth} />
     <Route exact path="/forgot" render={() => "password"} />
   </Switch>
-)
+);
 
 // class App extends Component {
 //   render() {
