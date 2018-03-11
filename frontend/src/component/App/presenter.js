bc54869258dce2731aa6146598a97bac5c6bb6f1
Yanghwa <kellerj87@hotmail.com>
2018-03-10 19:48:10 -0500

    add route and switch for app presenter

--- frontend/src/component/App/presenter.js
+++ frontend/src/component/App/presenter.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import styles from './styles.scss';
 import Footer from 'component/Footer';
 
-class App extends Component {
-  render() {
-    return (
-      <div className={styles.App}>
-        <Footer />
-      </div>
-    );
-  }
-}
+const App = props => [
+  props.isLoggedIn ? <PrivateRoute key={2} /> : <PublicRoutes key={2} />,
+  <Footer key={3} />
+]
+
+const PrivateRoute = props => (
+  <Switch>
+    <Route exact path="/" render={() => "feed"} />
+    <Route exact path="/explore" render={() => "explore"} />
+  </Switch>
+)
+
+const PublicRoutes = props => (
+  <Switch>
+    <Route exact path="/" render={() => "login"} />
+    <Route exact path="/forgot" render={() => "password"} />
+  </Switch>
+)
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className={styles.App}>
+//         <Switch>
+//           <Route exact path="/" render={() =>"hello"} />
+//           <Route path="/login" render={() => "login"} />
+//         </Switch>
+//         <Footer />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
