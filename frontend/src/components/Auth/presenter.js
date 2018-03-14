f72685b9be70b493f1199bf6d8c4d186ef9e9dd7
Yanghwa <kellerj87@hotmail.com>
2018-03-13 22:44:23 -0400

    add sign up and login form with css

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./styles.scss";
+import { LoginForm, SignupForm } from 'components/AuthForms';
 
 const Auth = (props, context) => (
   <main className={styles.auth}>
@@ -7,47 +8,69 @@ const Auth = (props, context) => (
       <img src={require("images/phone.png")} alt="Checkout our app. Is cool" />
     </div>
     <div className={styles.column}>
-      <div className={styles.whiteBox}>
-        {(() => {
-          switch (props.action) {
-            case "login":
-              return (
-                <p>
-                  Don't have an account?{" "}
-                  <span className={styles.changeLink} onClick={props.changeAction}>
-                    Sign up
-                  </span>
-                </p>
-              );
-            case "signup":
-              return (
-                <p>
-                  Have an account?{" "}
-                  <span className={styles.changeLink} onClick={props.changeAction}>
-                    Log in
-                  </span>
-                </p>
-              );
-            default:
-              return null;
-          }
-        })()}
-      </div>
-      <div className={styles.appBox}>
-        <span>Get the app</span>
-        <div className={styles.appstores}>
-          <img
-            src={require("images/ios.png")}
-            alt="Download it on the Apple Appstore"
-          />
-          <img
-            src={require("images/android.png")}
-            alt="Download it on the Apple Appstore"
-          />
+        <div className={`${styles.whiteBox} ${styles.formBox}`}>
+            <img src={require('images/logo.png')} alt='Logo' />
+            {props.action == 'login' && <LoginForm />}
+            {props.action == 'signup' && <SignupForm />}
         </div>
-      </div>
+        <div className={styles.whiteBox}>
+            {props.action == 'login' && (
+                <p className={styles.text}>
+                    Don't have an account?{" "}
+                    <span className={styles.changeLink} onClick={props.changeAction}>
+                    Sign up
+                    </span>
+                </p>
+            )}
+
+            {props.action == 'signup' && (
+                <p className={styles.text}>
+                    Have an account?{" "}
+                    <span className={styles.changeLink} onClick={props.changeAction}>
+                    Log in
+                    </span>
+                </p>
+            )}
+            {/* {(() => {
+            switch (props.action) {
+                case "login":
+                return (
+                    <p>
+                    Don't have an account?{" "}
+                    <span className={styles.changeLink} onClick={props.changeAction}>
+                        Sign up
+                    </span>
+                    </p>
+                );
+                case "signup":
+                return (
+                    <p>
+                    Have an account?{" "}
+                    <span className={styles.changeLink} onClick={props.changeAction}>
+                        Log in
+                    </span>
+                    </p>
+                );
+                default:
+                return null;
+            }
+            })()} */}
+        </div>
+        <div className={styles.appBox}>
+            <span>Get the app</span>
+            <div className={styles.appstores}>
+            <img
+                src={require("images/ios.png")}
+                alt="Download it on the Apple Appstore"
+            />
+            <img
+                src={require("images/android.png")}
+                alt="Download it on the Apple Appstore"
+            />
+            </div>
+        </div>
     </div>
-  </main>
+</main>
 );
 
 export default Auth;
