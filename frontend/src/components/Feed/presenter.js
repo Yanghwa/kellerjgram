ca733960524fc1f10df78bd5d3bda1c36937e88c
Yanghwa <kellerj87@hotmail.com>
2018-04-15 19:58:13 -0400

    feed photo setting 1.

--- frontend/src/components/Feed/presenter.js
+++ frontend/src/components/Feed/presenter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles.scss';
 import Loading from 'components/Loading';
+import FeedPhoto from 'components/FeedPhoto';
 
 const Feed = props => {
     if(props.loading) {
@@ -19,7 +20,8 @@ const LoadingFeed = props => (
 
 const RenderFeed = props => (
     <div className={styles.feed}>
-        {props.feed.map(post => post.caption)}
+        {props.feed.map(photo => <FeedPhoto {...photo} key={photo.id} />)}
+        {/* {props.feed.map(post => post.caption)} */}
     </div>
 );
 
