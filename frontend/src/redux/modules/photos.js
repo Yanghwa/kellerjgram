a63d090bd1cf664a8b0b6e3e63af72e472c7fdd4
Yanghwa <kellerj87@hotmail.com>
2018-04-16 17:57:38 -0400

    like and unlike changing state from front

--- frontend/src/redux/modules/photos.js
+++ frontend/src/redux/modules/photos.js
@@ -5,6 +5,8 @@ import { actionCreators as userActions } from 'redux/modules/user';
 //actions
 
 const SET_FEED = 'SET_FEED';
+const LIKE_PHOTO = 'LIKE_PHOTO';
+const UNLIKE_PHOTO = 'UNLIKE_PHOTO';
 
 //action creators
 
@@ -12,6 +14,20 @@ function setFeed(feed) {
     return {
         type: SET_FEED,
         feed
+    };
+}
+
+function doLikePhoto(photoId) {
+    return {
+        type: LIKE_PHOTO,
+        photoId
+    };
+}
+
+function doUnlikePhoto(photoId) {
+    return {
+        type: UNLIKE_PHOTO,
+        photoId
     };
 }
 
@@ -36,6 +52,12 @@ function getFeed() {
     };
 }
 
+function likePhoto(photoId) {
+    return (dispatch, getState) => {
+        dispatch(doLikePhoto(photoId));
+    };
+}
+
 //initial state
 
 const initialState = {};
@@ -46,11 +68,14 @@ function reducer(state = initialState, action) {
     switch (action.type) {
         case SET_FEED:
             return applySetFeed(state, action);
+        case LIKE_PHOTO:
+            return applyLikePhoto(state, action);
+        case UNLIKE_PHOTO:
+            return applyUnlikePhoto(state, action);
         default:
             return action;
     }
 }
-
 
 
 //reducer functions
@@ -61,6 +86,30 @@ function applySetFeed(state, action) {
         ...state,
         feed
     };
+}
+
+function applyLikePhoto(state, action) {
+    const { photoId } = action;
+    const { feed } = state;
+    const updateFeed = feed.map(photo => {
+        if(photo.id === photoId) {
+            return { ...photo, is_liked: true, like_count: photo.like_count+1}
+        }
+        return photo;
+    });
+    return { ...state, feed: updateFeed };
+}
+
+function applyUnlikePhoto(state, action) {
+    const { photoId } = action;
+    const { feed } = state;
+    const updateFeed = feed.map(photo => {
+        if(photo.id === photoId) {
+            return { ...photo, is_liked: false, like_count: photo.like_count-1}
+        }
+        return photo;
+    });
+    return { ...state, feed: updateFeed };
 }
 
 //exports
