1915c80fc5c823b975cdac827a7a8b089c597ac1
Yanghwa <kellerj87@hotmail.com>
2018-04-19 22:06:06 -0400

    set the userlist for likes

--- frontend/src/redux/modules/photos.js
+++ frontend/src/redux/modules/photos.js
@@ -8,6 +8,7 @@ const SET_FEED = 'SET_FEED';
 const LIKE_PHOTO = 'LIKE_PHOTO';
 const UNLIKE_PHOTO = 'UNLIKE_PHOTO';
 const ADD_COMMENT = 'ADD_COMMENT';
+const SET_PHOTO_LIKES = "SET_PHOTO_LIKES"
 
 //action creators
 
@@ -38,6 +39,14 @@ function addComment(photoId, comment) {
         photoId,
         comment
     }
+}
+
+function setPhotoLikes(photoId, likes) {
+    return {
+        type: SET_PHOTO_LIKES,
+        photoId,
+        likes
+    };
 }
 
 //api actions
@@ -124,6 +133,26 @@ function commentPhoto(photoId, message) {
     }
 }
 
+function getPhotoLikes(photoId) {
+    return (dispatch, getState) => {
+        const { user: { token } } = getState();
+        fetch(`/images/${photoId}/likes/`, {
+            headers: {
+            Authorization: `JWT ${token}`
+            }
+        })
+        .then(response => {
+        if (response.status === 401) {
+            dispatch(userActions.logout());
+        }
+            return response.json();
+        })
+        .then(json => {
+            dispatch(setPhotoLikes(photoId, json));
+        });
+    };
+}
+
 //initial state
 
 const initialState = {};
@@ -140,6 +169,8 @@ function reducer(state = initialState, action) {
             return applyUnlikePhoto(state, action);
         case ADD_COMMENT:
             return applyAddComment(state, action);
+        case SET_PHOTO_LIKES:
+            return applyPhotoLikes(state, action);
         default:
             return action;
     }
@@ -195,13 +226,29 @@ function applyAddComment(state, action) {
     return { ...state, feed: updateFeed };
 }
 
+function applyPhotoLikes(state, action) {
+    const { photoId, likes } = action;
+    const { feed } = state;
+    const updatedFeed = feed.map(photo => {
+    if (photo.id === photoId) {
+        return {
+        ...photo,
+        likes
+        };
+    }
+        return photo;
+    });
+    return { ...state, feed: updatedFeed };
+}
+
 //exports
 
 const actionCreators = {
     getFeed,
     likePhoto,
     unlikePhoto,
-    commentPhoto
+    commentPhoto,
+    getPhotoLikes
 };
 
 export { actionCreators };
