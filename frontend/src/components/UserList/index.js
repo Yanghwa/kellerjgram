fee33b2267ea53ae2474e49f88ceb256e4d34a38
Yanghwa <kellerj87@hotmail.com>
2018-04-20 23:31:28 -0400

    remove mapStateToProps from UserList

--- frontend/src/components/UserList/index.js
+++ frontend/src/components/UserList/index.js
@@ -1,12 +1,13 @@
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 import Container from "./container";
 
-const mapStateToProps = (state, ownProps) => {
-    // const { user: { userList } } = state;
-    const {  userList } = ownProps;
-    return {
-        userList
-    };
-};
+// const mapStateToProps = (state, ownProps) => {
+//     const { user: { userList } } = state;
+//     return {
+//         userList
+//     };
+// };
 
-export default connect(mapStateToProps)(Container);
+export default Container;
+
+// export default connect(mapStateToProps)(Container);
