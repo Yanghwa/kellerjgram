616b0681fe0369ef25124a67ce9c21a935ef1e4a
Yanghwa <kellerj87@hotmail.com>
2018-04-24 22:56:54 -0400

    add search and show userlist and imagelist

--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -7,6 +7,7 @@ const LOGOUT = 'LOGOUT'
 const SET_USER_LIST = 'SET_USER_LIST';
 const FOLLOW_USER = 'FOLLOW_USER';
 const UNFOLLOW_USER = 'UNFOLLOW_USER';
+const SET_IMAGE_LIST = 'SET_IMAGE_LIST';
 
 //action creators
 
@@ -34,14 +35,21 @@ function setFollowUser(userId) {
     return {
         type: FOLLOW_USER,
         userId
-    }
+    };
 }
 
 function setUnfollowUser(userId) {
     return {
         type: UNFOLLOW_USER,
         userId
-    }
+    };
+}
+
+function setImageList(imageList) {
+    return {
+        type: SET_IMAGE_LIST,
+        imageList
+    };
 }
 
 //API actions
@@ -196,6 +204,49 @@ function getExplore() {
     }
 }
 
+function searchByTerm(searchTerm) {
+    return async(dispatch, getState) => {
+        const { user : { token }} = getState();
+        const userList = await searchUsers(token, searchTerm);
+        const imageList = await searchImages(token, searchTerm);
+        if(userList === 401 || imageList === 401) {
+            dispatch(logout());
+        }
+        dispatch(setUserList(userList));
+        dispatch(setImageList(imageList));
+    };
+}
+
+function searchUsers(token, searchTerm) {
+    return fetch(`/users/search/?username=${searchTerm}`, {
+        headers: {
+            "Authorization": `JWT ${token}`
+        }
+    })
+    .then(response => {
+        if(response.status === 401) {
+            return 401;
+        }
+        return response.json();
+    })
+    .then(json => json);
+}
+
+function searchImages(token, searchTerm) {
+    return fetch(`/images/search/?hashtags=${searchTerm}`, {
+        headers: {
+            "Authorization": `JWT ${token}`
+        }
+    })
+    .then(response => {
+        if(response.status === 401) {
+            return 401;
+        }
+        return response.json();
+    })
+    .then(json => json);
+}
+
 //initial state
 const initialState = {
     isLoggedIn: localStorage.getItem('jwt') ? true : false,
@@ -215,6 +266,8 @@ function reducer(state = initialState, action) {
             return applyFollowUser(state, action);
         case UNFOLLOW_USER:
             return applyUnfollowUser(state, action);
+        case SET_IMAGE_LIST:
+            return applySetImageList(state, action);
         default:
             return state;
     }
@@ -271,6 +324,14 @@ function applyUnfollowUser(state, action) {
     return { ...state, userList: updatedUserList }
 }
 
+function applySetImageList(state, action) {
+    const {  imageList } = action;
+    return {
+        ...state,
+        imageList
+    };
+}
+
 //exports
 
 const actionCreators = {
@@ -281,7 +342,8 @@ const actionCreators = {
     getPhotoLikes,
     followUser,
     unfollowUser,
-    getExplore
+    getExplore,
+    searchByTerm
 };
 
 export { actionCreators };
